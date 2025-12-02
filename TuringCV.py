5b601804fdc388c89c9118a5f701274d7d84b42f
Lu-chh <chenghaolu044@gmail.com>
2025-12-02 19:22:57 +0800

    2

--- TuringCV.py
+++ TuringCV.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import cv2
 from matplotlib.font_manager import FontProperties
 
+
+# 画五角星
 def draw_star(canvas, center, r1, r2, color, thickness):
     cx, cy = center
     points = []
-    for i in range(10):
+    for i in range(10):                    #判断内角还是外角
         r = r1 if i % 2 == 0 else r2
         angle = np.deg2rad(i * 36 - 90)
         x = cx + r * np.cos(angle)
@@ -17,24 +19,27 @@ def draw_star(canvas, center, r1, r2, color, thickness):
 
 canvas = np.ones((800, 800, 3), np.uint8) * 255
 
+#画矩形
 cv2.rectangle(canvas, (10, 10), (100, 200), (0, 0, 255), 1)
 cv2.rectangle(canvas, (120, 20), (500, 200), (255, 0, 0), 1)
 cv2.rectangle(canvas, (300, 300), (400, 400), (0, 255, 0), -1)
 
+#画圆
 cv2.circle(canvas, (650, 400), 100, (0, 200, 200), 2)
 cv2.circle(canvas, (100, 600), 70, (255, 0, 255), -1)
 
+#画三角形
 triangle1 = np.array([[150, 100], [250, 50], [300, 150]], np.int32).reshape(-1, 1, 2)
 cv2.polylines(canvas, [triangle1], True, (0, 150, 200), 1)
-
 triangle2 = np.array([[550, 150], [650, 100], [700, 250]], np.int32).reshape(-1, 1, 2)
 cv2.polylines(canvas, [triangle2], True, (128, 0, 128), 1)
 
-src_points1 = np.float32([[0, 100], [200, 100], [100, 200]])
+#画椭圆
+src_points1 = np.float32([[0, 100], [200, 100], [100, 200]])      #仿射变换1
 dst_points1 = np.float32([[0, 100], [200, 100], [100, 150]])
 M1 = cv2.getAffineTransform(src_points1, dst_points1)
 
-src_points2 = np.float32([[0, 100], [200, 100], [100, 200]])
+src_points2 = np.float32([[0, 100], [200, 100], [100, 200]])      #仿射变换2
 dst_points2 = np.float32([[0, 100], [100, 100], [100, 200]])
 M2 = cv2.getAffineTransform(src_points2, dst_points2)
 
@@ -58,38 +63,55 @@ for angle in np.linspace(0, 2*np.pi, 600):
 ellipse_points = np.array(oval2, np.int32).reshape(-1, 1, 2)
 cv2.polylines(canvas, [ellipse_points], True, (114, 51, 4), 1)
 
+#画五角星
 draw_star(canvas,(400, 625),200,80,(0, 0, 255),1)
 
+#截取模板
 template = canvas[10:201, 10:101]
 cv2.imwrite("template.png", template)
+
+#赋值画板
 matching = canvas.copy()
 contours = canvas.copy()
 
+# 预处理模板并获得模板的轮廓
 template_img = cv2.imread("template.png", 0)
 _, template_thresh = cv2.threshold(template_img, 127, 255, cv2.THRESH_BINARY)
 template_cnts, _ = cv2.findContours(template_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 template_cnt = template_cnts[0]
 
+#预处理画板
 gray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 100, 200)
 
+#闭运算 处理椭圆断裂
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
 closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
 
-cnts, hierarchy = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#获取轮廓
+cnts, hierarchy = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #部分轮廓存在两层 需要筛选内外层
 
+#存放不重复部分
 true_cnts = []
+
+#对每个轮廓进行筛选和判断
 for i, h in enumerate(hierarchy[0]):
     parent = h[3]
     if parent == -1 or parent == 15:
         true_cnts.append(cnts[i])
+
+        #获取每个轮廓的近似
         epsilon = 0.02 * cv2.arcLength(cnts[i], True)
         approx = cv2.approxPolyDP(cnts[i], epsilon, True)
+
+        #获取圆度
         area = cv2.contourArea(cnts[i])
         perimeter = cv2.arcLength(cnts[i], True)
         circularity = 4 * np.pi * area / (perimeter ** 2)
 
         match_score = cv2.matchShapes(cnts[i], template_cnt, cv2.CONTOURS_MATCH_I1, 0)
+
+        #判断是否与模板匹配
         if match_score > 0.2:
             x, y, w, h = cv2.boundingRect(cnts[i])
             cv2.rectangle(matching, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -104,6 +126,7 @@ for i, h in enumerate(hierarchy[0]):
 
             cv2.putText(matching, text, position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness)
 
+        #判断每个轮廓具体种类
         if len(approx) == 3:
             shape_type = "triangle"
         elif len(approx) == 4:
--- pose.py
+++ pose.py
@@ -5,14 +5,13 @@ import mediapipe as mp
 from matplotlib.font_manager import FontProperties
 import csv
 
-font = FontProperties(fname="C:/Windows/Fonts/simhei.ttf", size=9)
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 video_path = r"pose.mp4"
 cap = cv2.VideoCapture(video_path)
 
-
+#得到图像尺寸和视频帧率
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -21,17 +20,21 @@ print("FPS:", fps)
 print("宽度:", width)
 print("高度:", height)
 
+#设定处理后视频格式
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = cv2.VideoWriter("new_pose.mp4", fourcc, fps, (width, height))
 
+#预设定repl.csv
 csv_file = open("repl.csv", "w", newline="", encoding="utf-8")
 csv_writer = csv.writer(csv_file)
-
 csv_writer.writerow(["frame", "index", "x", "y", "z", "visibility"])
 
-total_v = 0
-frame_idx = 0
-last = None
+
+total_v = 0    #每一帧速度的和
+frame_idx = 0  #视频帧编号
+last = None    #上一帧的关键点
+
+#处理每一帧
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
@@ -40,11 +43,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         frame_idx += 1
 
+        #获取关键点
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
-
         results = pose.process(image)
 
+        #绘制关键点
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -53,34 +57,38 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                                  )
 
+        #获取关键点集
         landmarks = results.pose_landmarks.landmark
 
+        #将关键点位置写入repl.csv
         for i, lm in enumerate(landmarks):
             csv_writer.writerow([frame_idx, i, lm.x, lm.y, lm.z, lm.visibility])
 
+        #判断是否是第0帧
         if last == None:
             last = landmarks[25]
         else:
-            lk = landmarks[25]
-            lh = landmarks[23]
-            lfi = landmarks[31]
+            lk = landmarks[25] #左膝位置
 
-            ds = (((lk.x - last.x) ** 2 + (lk.y - last.y) ** 2) ** 0.5) * 3.7
-            v = ds * fps * 2
-            total_v += v
-            cv2.putText(image,
+            ds = (((lk.x - last.x) ** 2 + (lk.y - last.y) ** 2) ** 0.5) * 3.7 #一帧的位移
+            v = ds * fps * 2                                                  #计算速度
+            total_v += v                                                      #累加计入速度和 用于求平均速度
+
+            cv2.putText(image,                                                #在图中标注每一帧速度
                         f"Speed: {v:.2f} m/s",
                         (1000, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1,
                         (0, 0, 0), 2)
-            last = landmarks[25]
+            last = landmarks[25]                                              #将本节点记为last
 
+        #写入new_pose.mp4
         out.write(image)
-
+        #展示处理后的视频
         cv2.imshow("MediaPipe Feed", image)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
+    #输出平均速度
     print(f"mean_v:{total_v / frame_idx:.2f}m/s")
     cap.release()
     cv2.destroyAllWindows()
